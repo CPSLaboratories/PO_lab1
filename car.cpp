ff05fa47bdcef57bc33e750b0fd69c1ed17d2fb7
CPSLaboratories <przemkopp@gmail.com>
2021-04-16 21:37:29 +0200

    Utworzone wszystkie pliki

---
+++ car.cpp
@@ -0,0 +1,3 @@
+
+/*-----------------Programowanie Obiektowe/AiR/Semestr 2/Grupa 5/Sekcja 19------------------------------
+ ------------------Przemys³aw Papiernik/Jakub Wrzeszcz/Sebastian Likoñski-----------------------------*/
--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+
+/*-----------------Programowanie Obiektowe/AiR/Semestr 2/Grupa 5/Sekcja 19------------------------------ 
+ ------------------Przemys³aw Papiernik/Jakub Wrzeszcz/Sebastian Likoñski-----------------------------*/
+
 main()
 {
 	//test
